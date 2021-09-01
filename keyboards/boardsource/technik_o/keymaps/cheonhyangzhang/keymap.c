c1bcf84a6d7142e340fa8e87521fe62a186cf1e9
Tianxiang Zhang <cenhiangapply@gmail.com>
2021-08-31 21:56:33 -0700

    fix technik a and z

--- keyboards/boardsource/technik_o/keymaps/cheonhyangzhang/keymap.c
+++ keyboards/boardsource/technik_o/keymaps/cheonhyangzhang/keymap.c
@@ -28,9 +28,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //,--------------------------------------.            ,--------------------------------------------|
         KC_NO,  KC_Q,  KC_W, KC_E, KC_R, KC_T,          KC_Y, KC_U, TD(I_QUOT),    TD(O_LSFT_QUOT),   KC_P,    KC_NO,
     //|--------+--------+--------+--------+---------|     |--------+--------+--------+--------+--------|
-        KC_Q, TD(A_CTL), KC_S, KC_D, KC_F, KC_G,    KC_H, KC_J, KC_K,    KC_L,   KC_ENT,  KC_P,
+        KC_Q, KC_A, KC_S, KC_D, KC_F, KC_G,    KC_H, KC_J, KC_K,    KC_L,   KC_ENT,  KC_P,
     //|--------+--------+--------+--------+---------|     |--------+--------+--------+--------+--------|
-        KC_LGUI, TD(Z_SFT), KC_X, KC_C, KC_V, KC_B,    KC_N, KC_M, KC_COMM, KC_DOT, TD(SLASH_RSHIFT), TO(4),
+        KC_LGUI, KC_Z, KC_X, KC_C, KC_V, KC_B,    KC_N, KC_M, KC_COMM, KC_DOT, TD(SLASH_RSHIFT), TO(4),
     //|--------+--------+--------+--------+---------|     |--------+--------+--------+--------+--------|
         KC_NO, KC_NO, KC_NO,   MO(3),   KC_LSFT,  MO(1),  KC_SPC,   MO(2), TO(5), KC_NO, KC_NO, KC_NO
                             //`--------------------------'  `----------------------'
