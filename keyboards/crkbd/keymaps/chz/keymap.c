1f5b7a6af912dcddcc40c91f23badc69723e4f31
Tianxiang Zhang <cenhiangapply@gmail.com>
2023-10-24 12:02:25 -0700

    Try home row mod

--- keyboards/crkbd/keymaps/chz/keymap.c
+++ keyboards/crkbd/keymaps/chz/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // KC_RSHIFT is not often used
   [0] = LAYOUT_split_3x6_3(
     KC_LALT,  KC_LGUI,  KC_W, KC_E,  KC_R,    KC_T,    KC_Y,   KC_U,  KC_I,       KC_O,            KC_NO,    KC_BSPC,
-    KC_Q,     KC_A,     KC_S, KC_D,  KC_F,    KC_G,    KC_H,   KC_J,  KC_K,       KC_L,            KC_ENT,   KC_P,
+    KC_Q,     LALT_T(KC_A),   LCTL_T(KC_S), LGUI_T(KC_D),  LSFT_T(KC_F),    KC_G,    KC_H,   LSFT_T(KC_J),  LGUI_T(KC_K),       LCTL_T(KC_L),            LALT_T(KC_ENT),   KC_P,
     KC_LCTL,  KC_Z,     KC_X, KC_C,  KC_V,    KC_B,    KC_N,   KC_M,  KC_COMM,    KC_DOT,          KC_SLASH, KC_QUOT,
                               MO(4), KC_LSFT, MO(2),   KC_SPC, MO(3), KC_NO
   ),
