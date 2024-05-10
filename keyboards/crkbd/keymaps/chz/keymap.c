090dcb50b11e7e55752d85d7c4085e87e5c27a16
Tianxiang Zhang <cenhiangapply@gmail.com>
2024-05-10 09:31:54 -0700

    Use d as the to base layer from mouse layer is not very comfortable Hence try to switch to the f location to do it. Also realize cmd is more frequently used than ctrl, hence probably makes more sense to use f location for cmd and d location for ctrl, lets try this.

--- keyboards/crkbd/keymaps/chz/keymap.c
+++ keyboards/crkbd/keymaps/chz/keymap.c
@@ -9,6 +9,7 @@ enum layer_names {
     _BASE,
     _MOUSE,
     _CMD,
+    _CMD_MOUSE,
     _NAV,
     _NUM,
     _WIN,
@@ -19,19 +20,25 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_split_3x6_3(
     MO(_BOOT),      KC_NO,  KC_W,           KC_E,         KC_R,         KC_T,       KC_Y,   KC_U,          KC_I,         KC_O,           KC_NO,     MO(_BOOT),
-    KC_Q,           KC_A,   LALT_T(KC_S),   LGUI_T(KC_D), LCTL_T(KC_F), KC_G,       KC_H,   LCTL_T(KC_J),  LGUI_T(KC_K), LALT_T(KC_L),   KC_P,      KC_RSFT,
+    KC_Q,           KC_A,   LALT_T(KC_S),   LCTL_T(KC_D), LGUI_T(KC_F), KC_G,       KC_H,   LCTL_T(KC_J),  LALT_T(KC_K), LGUI_T(KC_L),   KC_P,      KC_RSFT,
     KC_CAPS_LOCK,   KC_Z,   KC_X,           KC_C,         KC_V,         KC_B,       KC_N,   KC_M,          KC_COMM,      KC_DOT,         KC_QUOT,   KC_NO,
                                             MO(_NUM),     KC_LSFT,      MO(_CMD),   KC_SPC, MO(_NAV),      KC_NO
   ),
   [_MOUSE] = LAYOUT_split_3x6_3(
-      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_NO,             KC_NO,          KC_MS_UP,       KC_NO,          KC_NO,      KC_NO,
-      KC_NO,    KC_NO,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_NO,       KC_MS_WH_DOWN,     KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_MS_BTN2, KC_NO,
-      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_MS_WH_UP,       KC_MS_WH_RIGHT, KC_SPC,         KC_MS_WH_LEFT,  KC_NO,      KC_NO,
-                                    KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_MS_BTN1,        KC_TRNS,        KC_NO
+      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,              KC_NO,             KC_NO,          KC_MS_UP,       KC_NO,          KC_NO,      KC_NO,
+      KC_NO,    KC_NO,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_NO,              KC_MS_WH_DOWN,     KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_MS_BTN2, KC_NO,
+      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,              KC_MS_WH_UP,       KC_MS_WH_RIGHT, KC_SPC,         KC_MS_WH_LEFT,  KC_NO,      KC_NO,
+                                    KC_TRNS,  KC_TRNS,  MO(_CMD_MOUSE),     KC_MS_BTN1,        KC_TRNS,        KC_NO
   ),
   [_CMD] = LAYOUT_split_3x6_3(
     LCTL(LGUI(KC_Q)),   KC_NO,        LSFT(LGUI(KC_Z)),   LGUI(KC_W),   LGUI(KC_R),     LGUI(KC_T),     KC_CIRC,      KC_AMPR,      KC_LPRN,            KC_RPRN,        KC_NO,    KC_NO,
-    LGUI(KC_TAB),       LGUI(KC_A),   KC_TAB,             TO(_BASE),    KC_ESC,         LGUI(KC_F),     KC_ASTR,      KC_BSPC,      LCTL(KC_X),         KC_MINUS,       KC_COLN,  KC_RSFT,
+    LGUI(KC_TAB),       LGUI(KC_A),   KC_TAB,             KC_NO,        KC_ESC,         LGUI(KC_F),     KC_ASTR,      KC_BSPC,      LCTL(KC_X),         KC_MINUS,       KC_COLN,  KC_RSFT,
+    LSFT(KC_TAB),       LGUI(KC_Z),   LGUI(KC_X),         LCTL(KC_C),   LGUI(KC_C),     LGUI(KC_V),     KC_NO,        KC_LBRC,      KC_RBRC,            LSFT(KC_MINUS), KC_SCLN,  KC_NO,
+                                                          KC_NO,        KC_NO,          KC_NO,          KC_ENT,       MO(_NAV),     KC_NO
+  ),
+  [_CMD_MOUSE] = LAYOUT_split_3x6_3(
+    LCTL(LGUI(KC_Q)),   KC_NO,        LSFT(LGUI(KC_Z)),   LGUI(KC_W),   LGUI(KC_R),     LGUI(KC_T),     KC_CIRC,      KC_AMPR,      KC_LPRN,            KC_RPRN,        KC_NO,    KC_NO,
+    LGUI(KC_TAB),       LGUI(KC_A),   KC_TAB,             KC_NO,        TO(_BASE),      LGUI(KC_F),     KC_ASTR,      KC_BSPC,      LCTL(KC_X),         KC_MINUS,       KC_COLN,  KC_RSFT,
     LSFT(KC_TAB),       LGUI(KC_Z),   LGUI(KC_X),         LCTL(KC_C),   LGUI(KC_C),     LGUI(KC_V),     KC_NO,        KC_LBRC,      KC_RBRC,            LSFT(KC_MINUS), KC_SCLN,  KC_NO,
                                                           KC_NO,        KC_NO,          KC_NO,          KC_ENT,       MO(_NAV),     KC_NO
   ),
