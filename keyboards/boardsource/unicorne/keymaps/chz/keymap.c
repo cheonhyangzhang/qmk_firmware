226993e5958afc4d3eee6a682e3f4b739b0b20e0
Tianxiang Zhang <cenhiangapply@gmail.com>
2024-06-01 10:32:01 -0700

    Avoid home row keys in mouse layer

--- keyboards/boardsource/unicorne/keymaps/chz/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/chz/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
   ),
   [_MOUSE] = LAYOUT_split_3x6_3(
       KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_NO,             KC_NO,          KC_MS_UP,       KC_NO,          KC_NO,      KC_NO,
-      KC_NO,    KC_NO,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_NO,       KC_MS_WH_DOWN,     KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_MS_BTN2, KC_NO,
+      KC_NO,    KC_NO,    KC_LALT,  KC_LGUI,  KC_LCTL,  KC_NO,       KC_MS_WH_DOWN,     KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_MS_BTN2, KC_NO,
       KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_MS_WH_UP,       KC_MS_WH_RIGHT, KC_SPC,         KC_MS_WH_LEFT,  KC_NO,      KC_NO,
                                     KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_MS_BTN1,        KC_TRNS,        KC_NO
   ),
