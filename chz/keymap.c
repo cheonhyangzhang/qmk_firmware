559e19788ffe22258d231df13e1ba3a1e1b406fe
Tianxiang Zhang <cenhiangapply@gmail.com>
2025-06-16 16:11:35 -0700

    add key for raycast search snippet

--- chz/keymap.c
+++ chz/keymap.c
@@ -34,8 +34,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
   [2] = LAYOUT_split_3x6_3(
     KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,          KC_NO,      KC_NO,      KC_NO,          KC_NO,
     KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_LEFT),  KC_UP,      LCTL(KC_RIGHT), KC_NO,          KC_NO,
-    KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT,        KC_DOWN,    KC_RIGHT,       KC_NO,  KC_NO,
-    KC_NO, KC_NO, KC_TRNS, KC_SPC, LGUI(KC_BSPC), KC_NO
+    KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, LGUI(KC_Z), KC_LEFT,        KC_DOWN,    KC_RIGHT,       LGUI(KC_BSPC),  KC_NO,
+    KC_NO, KC_NO, KC_TRNS, KC_SPC, KC_TRNS, KC_NO
   ),
 
   [3] = LAYOUT_split_3x6_3(
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
                                                           KC_NO,        KC_NO,          KC_NO,          KC_ENT,             MO(4),     KC_NO
   ),
   [4] = LAYOUT_split_3x6_3(
-     LCTL(KC_F3),             KC_NO,              KC_NO,              KC_LCBR,        KC_RCBR,    KC_NO,              LGUI(KC_GRV),       KC_NO,      KC_UP,      KC_EQUAL,     KC_NO,      KC_NO,
+     LCTL(KC_F3),             KC_NO,              KC_NO,              KC_LCBR,        KC_RCBR,    KC_NO,              LGUI(KC_GRV),     LALT(LGUI(KC_Y)),   KC_UP,      KC_EQUAL,     KC_NO,      KC_NO,
      KC_NO,             LGUI(KC_1),         LSFT(LCTL(KC_TAB)), LCTL(KC_TAB),   KC_DLR,     KC_PERC,    LSFT(LGUI(KC_C)),   KC_LEFT,    KC_DOWN,    KC_RGHT,      LGUI(KC_Y), LGUI(KC_BSPC),
      LGUI(LSFT(KC_T)),  KC_EXLM,            KC_AT,          KC_HASH,    KC_TILD,            KC_GRV,     LSFT(LGUI(KC_N)),   KC_BSLS,    TO(1), KC_SLASH,     KC_NO,      TO(2),
                                                             KC_NO,          KC_LSFT,    MO(3),   KC_NO,              KC_NO,      KC_NO
