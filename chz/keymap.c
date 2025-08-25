2b2211933e23bf4539fa4f809054de0b5cc91c91
Tianxiang Zhang <cenhiangapply@gmail.com>
2025-08-25 10:40:36 -0700

    re-add brightness after fixing the compile issues

--- chz/keymap.c
+++ chz/keymap.c
@@ -52,8 +52,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
   ),
   [5] = LAYOUT_split_3x6_3(
       LGUI(KC_BSPC),    KC_NO,          LCTL(LSFT(KC_PGUP)),      LGUI(LSFT(KC_5)),         LCTL(LSFT(KC_PGDN)),      KC_NO,              KC_NO,          KC_7, KC_8,   KC_9, KC_NO,   KC_KB_MUTE,
-      KC_NO,  KC_NO,    LGUI(KC_LBRC),  LCTL(LGUI(LSFT(KC_4))),   LGUI(KC_RBRC),  LSFT(LGUI(KC_G)),   KC_PGUP,        KC_4, KC_5,   KC_6, KC_DOT,  KC_KB_VOLUME_UP,
-      KC_NO,  KC_NO,    KC_NO,          LGUI(LSFT(KC_4)),         KC_NO,          KC_NO,              KC_PGDN,        KC_1, KC_2,   KC_3, KC_NO,   KC_KB_VOLUME_DOWN,
+      KC_BRIGHTNESS_UP,  KC_NO,    LGUI(KC_LBRC),  LCTL(LGUI(LSFT(KC_4))),   LGUI(KC_RBRC),  LSFT(LGUI(KC_G)),   KC_PGUP,        KC_4, KC_5,   KC_6, KC_DOT,  KC_KB_VOLUME_UP,
+      KC_BRIGHTNESS_DOWN,  KC_NO,    KC_NO,          LGUI(LSFT(KC_4)),         KC_NO,          KC_NO,              KC_PGDN,        KC_1, KC_2,   KC_3, KC_NO,   KC_KB_VOLUME_DOWN,
                                         KC_NO,                    KC_NO,          KC_NO,              LSFT(KC_ENT),   KC_0, KC_NO
   ),
   [6] = LAYOUT_split_3x6_3(
